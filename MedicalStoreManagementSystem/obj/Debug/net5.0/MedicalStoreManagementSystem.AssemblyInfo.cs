2c47fd4605a98f84e5bab558c376eb7f520802aa
TanviM <96225437+M-Tanvi@users.noreply.github.com>
2022-02-05 22:58:25 +0530

    docker

--- MedicalStoreManagementSystem/obj/Debug/net5.0/MedicalStoreManagementSystem.AssemblyInfo.cs
+++ MedicalStoreManagementSystem/obj/Debug/net5.0/MedicalStoreManagementSystem.AssemblyInfo.cs
@@ -11,6 +11,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("d8d2dc2e-cb87-42fb-95ff-c9a0ab1bde97")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("MedicalStoreManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
